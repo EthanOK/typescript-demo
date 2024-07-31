906d92b47150f8b67519bf025d2b294db4648c8f
DJ.Zhang <dengji@evg.co>
2024-07-31 16:10:22 +0800

    modified:   src/safe/tools.ts

--- src/safe/tools.ts
+++ src/safe/tools.ts
@@ -13,6 +13,16 @@ export const initSafe = async () => {
     safeAddress: config.SAFE_ADDRESS,
   });
 };
+
+// TODO: metamask use initSafeUseWallet
+// export const initSafeUseWallet = async () => {
+//   return await Safe.init({
+//     provider: window.ethereum, // Or any compatible EIP-1193 provider
+//     signer: "0xSignerAddressOrPrivateKey", // Signer address or private key
+//     safeAddress: "0xSafeAddress",
+//   });
+// };
+
 const safeTransactionData: MetaTransactionData = {
   to: config.TO,
   value: config.VALUE,
